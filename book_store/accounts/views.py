285799caccc6e9efcacae5145708f0382d9b2b59
Damun <damunjamiri@gmail.com>
2020-10-31 16:05:14 +0330

    create login view for accounts app

--- book_store/accounts/views.py
+++ book_store/accounts/views.py
@@ -1,3 +1,24 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render
+from .form import  LoginForm
 
-# Create your views here.
+def login_page(request):
+    form = LoginForm(request.POST or None)
+    print(request.user.is_authenticated)
+    context = {
+        'title': 'Login',
+        "header": 'login',
+        "form": form,
+    }
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            context["form"] = LoginForm
+        else:
+            pass
+
+    return render(request, 'accounts/login.html', context)
