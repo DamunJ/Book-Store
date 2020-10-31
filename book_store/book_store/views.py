db61f20cadeffa09da1f36db360b6ea007eef83a
amirhossein-ka <amirhossein.karimi9@gmail.com>
2020-10-31 12:59:27 +0330

    fix static files

--- book_store/book_store/views.py
+++ book_store/book_store/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 def home_page(request):
     context = {
         'title': 'Home page',
-        'desciption': 'simple home page '
+        'description': 'simple home page '
     }
 
     return render(request, 'homepage.html', context)
