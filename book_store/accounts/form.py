b58847c402c67cf1f47f40ff69a5c33350424319
amirhossein-ka <amirhossein.karimi9@gmail.com>
2020-11-02 17:57:53 +0330

    add register

--- book_store/accounts/form.py
+++ book_store/accounts/form.py
@@ -1,10 +1,52 @@
 from django import forms
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class LoginForm(forms.Form):
     username = forms.CharField(
-        widget=forms.TextInput(attrs={'class': 'signup__input', 'name': 'username','id': 'username'})
+        widget=forms.TextInput(attrs={'class': 'signup__input', 'name': 'username', 'id': 'username'})
     )
     password = forms.CharField(
-        widget=forms.PasswordInput(attrs={'class': 'signup__input', 'name': 'username','id': 'username'})
+        widget=forms.PasswordInput(attrs={'class': 'signup__input', 'name': 'username', 'id': 'username'})
     )
+
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'signup__input', 'name': 'username', 'id': 'username'}))
+
+    email = forms.CharField(
+        widget=forms.EmailInput(attrs={'class': 'signup__input', 'name': 'username', 'id': 'username'}))
+
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'signup__input', 'name': 'username', 'id': 'username'})
+    )
+
+    confirm_password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'signup__input', 'name': 'username', 'id': 'username'})
+    )
+
+    def clean(self):
+        data = self.cleaned_data
+        password = self.cleaned_data.get('password')
+        conf_pass = self.cleaned_data.get('confirm_password')
+        if password != conf_pass:
+            raise forms.ValidationError("رمز عبور یکسان نیست ")
+
+        return data
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        qs = User.objects.filter(username=username)
+        if qs.exists():
+            raise forms.ValidationError("نام کاربری وجود دارد ")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        qs = User.objects.filter(email=email)
+        if qs.exists():
+            raise forms.ValidationError("email is already taken")
+        return email
--- book_store/accounts/views.py
+++ book_store/accounts/views.py
@@ -1,8 +1,9 @@
-from django.contrib.auth import authenticate, login
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, get_user_model
+from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
-from .form import  LoginForm
+from .form import LoginForm, RegisterForm
 from django.urls import reverse
+
 
 def login_page(request):
     form = LoginForm(request.POST or None)
@@ -10,6 +11,8 @@ def login_page(request):
     context = {
         "form": form,
     }
+    if request.user.is_authenticated:  # چک میکنه ببینه یوزر لاگین شده یا نه (:
+        return redirect('/')
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -23,4 +26,23 @@ def login_page(request):
                 "form": form,
                 "error": "کاربری با این مشخصات یافت نشد!",
             }
+
     return render(request, 'accounts/login.html', context)
+
+
+User = get_user_model()
+
+
+def register(request):
+    form = RegisterForm(request.POST or None)
+    if request.user.is_authenticated:
+        return redirect('/')
+    context = {'form': form}
+    if form.is_valid():
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        new_user = User.objects.create_user(username=username, password=password, email=email)
+        print(new_user)
+        return redirect('/')
+    return render(request, 'accounts/register.html', context)
